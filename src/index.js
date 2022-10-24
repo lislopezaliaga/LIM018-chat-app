90ddd48ee0dab66fce48f25e311bc5cb9164ae42
elisabethfarfan <elisabethfarfan@gmail.com>
2022-10-24 09:54:00 -0500

    update cloudinary

--- src/index.js
+++ src/index.js
@@ -20,16 +20,16 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-app.use(cors());
-app.delete('/:public_id', async (req, res) => {
-  const { public_id } = req.params;
-  try {
-    await cloudinary.uploader.destroy(public_id);
-    res.status(200).send();
-  } catch (error) {
-    res.status(400).send();
-  }
-});
+// app.use(cors());
+// app.delete('/:public_id', async (req, res) => {
+//   const { public_id } = req.params;
+//   try {
+//     await cloudinary.uploader.destroy(public_id);
+//     res.status(200).send();
+//   } catch (error) {
+//     res.status(400).send();
+//   }
+// });
 
 // importamos la ruta
 const routingRoutes = require('./routes/routing.routes');
