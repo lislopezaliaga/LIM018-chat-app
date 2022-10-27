e3b83c7374854d4f9bb27b5bd5108e29a2c09e8e
elisabethfarfan <elisabethfarfan@gmail.com>
2022-10-27 12:25:41 -0500

    update credentials client

--- src/index.js
+++ src/index.js
@@ -31,6 +31,13 @@ app.use(
     origin: 'https://chatowlapp.onrender.com',
   })
 );
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use(morgan('dev'));
 app.use(express.json());
