0917efb664b765f5040d2ba194fe0262ef2bba62
elisabethfarfan <elisabethfarfan@gmail.com>
2022-10-27 17:12:11 -0500

    update credentials client

--- src/index.js
+++ src/index.js
@@ -44,10 +44,10 @@ app.use(express.json());
 
 app.use(routingRoutes);
 app.use((err, req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  // res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  // res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   return res.json({
     message: err.message,
   });
